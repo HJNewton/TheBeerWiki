4388c5e54ebaed2a6e6fc74d827903bb42cb852c
Harry <harry3.newton@live.uwe.ac.uk>
2021-04-16 20:20:04 +0100

    Finalised first info page

--- app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingFragment.kt
+++ app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingFragment.kt
@@ -5,9 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
+import androidx.lifecycle.Observer
 import com.example.thebeerwiki.R
 
 class CarlingFragment : Fragment()
@@ -26,6 +27,9 @@ class CarlingFragment : Fragment()
         // CARLING
         val carlingImage: ImageView = root.findViewById(R.id.carling_image)
         carlingImage.setImageResource(R.drawable.carling_image)
+
+        val carlingText: TextView = root.findViewById(R.id.carling_trivia_text)
+        carlingViewModel.aboutText.observe(viewLifecycleOwner, Observer { carlingText.text = it })
 
         return root
     }
--- app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingViewModel.kt
+++ app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingViewModel.kt
@@ -6,5 +6,14 @@ import androidx.lifecycle.ViewModel
 
 class CarlingViewModel : ViewModel()
 {
-
+    private val _aboutText = MutableLiveData<String>().apply {
+        value = "Carling was first introduced to the U.K in 1952 under the name: Carling Black Label as a bottle-only beer. " +
+                "\n" +
+                "\nIt was first offered on draught in The Hill Top, Sheffield in 1965." +
+                "\n" +
+                "\nSince 1985, Carling has been the best selling beer throughout the entirety of Britain" +
+                "\n" +
+                "\nIn 1997 'Black Label' was no longer used in Britain, however it is still known as such in Canada and South Africa."
+    }
+    val aboutText: LiveData<String> = _aboutText
 }
