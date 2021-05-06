2a4a1155e05b893cbdbb38eb37d860006e31dd76
Harry <harry3.newton@live.uwe.ac.uk>
2021-05-06 01:33:18 +0100

    3/6 pages are refactored to new style

--- app/src/main/java/com/example/thebeerwiki/ui/beers/CarlsbergFragment.kt
+++ app/src/main/java/com/example/thebeerwiki/ui/beers/CarlsbergFragment.kt
@@ -1,9 +1,11 @@
 package com.example.thebeerwiki.ui.beers
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -24,12 +26,24 @@ class CarlsbergFragment : Fragment()
             ViewModelProvider(this).get(CarlsbergViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_carlsberg, container, false)
 
-        val carlsbergImage: ImageView = root.findViewById(R.id.carlsberg_image)
-        carlsbergImage.setImageResource(R.drawable.carlsberg_image)
+        val sharedPref = activity?.getSharedPreferences("carlsberg_drunk", Context.MODE_PRIVATE)
+        var drunk = sharedPref?.getInt("carlsberg_drunk", 0)
 
-        val carlsbergText: TextView = root.findViewById(R.id.carlsberg_trivia_text)
-        carlsbergViewModel.aboutText.observe(viewLifecycleOwner, Observer { carlsbergText.text = it })
-        
+        val drunk_text: TextView = root.findViewById(R.id.carlsberg_drunk_text)
+        drunk_text.setText(sharedPref?.getInt("carlsberg_drunk", 0).toString())
+
+        val carlingPlusButton: Button = root.findViewById(R.id.plus_carlsberg)
+
+        carlingPlusButton.setOnClickListener()
+        {
+            if (drunk != null)
+            {
+                drunk = drunk!! + 1
+                sharedPref?.edit()?.putInt("carlsberg_drunk", drunk!!)?.apply()
+                drunk_text.setText(sharedPref?.getInt("carlsberg_drunk", 0).toString())
+            }
+        }
+
         return root
     }
 }
--- app/src/main/java/com/example/thebeerwiki/ui/beers/FostersFragment.kt
+++ app/src/main/java/com/example/thebeerwiki/ui/beers/FostersFragment.kt
@@ -1,9 +1,11 @@
 package com.example.thebeerwiki.ui.beers
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -24,11 +26,23 @@ class FostersFragment : Fragment()
             ViewModelProvider(this).get(FostersViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_fosters, container, false)
 
-        val fostersImage: ImageView = root.findViewById(R.id.fosters_image)
-        fostersImage.setImageResource(R.drawable.fosters_image)
+        val sharedPref = activity?.getSharedPreferences("fosters_drunk", Context.MODE_PRIVATE)
+        var drunk = sharedPref?.getInt("fosters_drunk", 0)
 
-        val fostersText: TextView = root.findViewById(R.id.fosters_trivia_text)
-        fostersViewModel.aboutText.observe(viewLifecycleOwner, Observer { fostersText.text = it })
+        val drunk_text: TextView = root.findViewById(R.id.fosters_drunk_text)
+        drunk_text.setText(sharedPref?.getInt("fosters_drunk", 0).toString())
+
+        val carlingPlusButton: Button = root.findViewById(R.id.plus_fosters)
+
+        carlingPlusButton.setOnClickListener()
+        {
+            if (drunk != null)
+            {
+                drunk = drunk!! + 1
+                sharedPref?.edit()?.putInt("fosters_drunk", drunk!!)?.apply()
+                drunk_text.setText(sharedPref?.getInt("fosters_drunk", 0).toString())
+            }
+        }
         
         return root
     }
