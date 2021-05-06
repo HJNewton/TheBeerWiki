bdfe5036caa651d3a7f9e7c31229b276a031199e
Harry <harry3.newton@live.uwe.ac.uk>
2021-05-06 01:11:34 +0100

    redid layout so its cool

--- app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingFragment.kt
+++ app/src/main/java/com/example/thebeerwiki/ui/beers/CarlingFragment.kt
@@ -1,15 +1,20 @@
 package com.example.thebeerwiki.ui.beers
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
 import com.example.thebeerwiki.R
+import org.w3c.dom.Text
 
 class CarlingFragment : Fragment()
 {
@@ -24,11 +29,23 @@ class CarlingFragment : Fragment()
             ViewModelProvider(this).get(CarlingViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_carling, container, false)
 
-        val carlingImage: ImageView = root.findViewById(R.id.carling_image)
-        carlingImage.setImageResource(R.drawable.carling_image)
+        val sharedPref = activity?.getSharedPreferences("carlings_drunk", Context.MODE_PRIVATE)
+        var drunk = sharedPref?.getInt("carlings_drunk", 0)
 
-        val carlingText: TextView = root.findViewById(R.id.carling_trivia_text)
-        carlingViewModel.aboutText.observe(viewLifecycleOwner, Observer { carlingText.text = it })
+        val drunk_text: TextView = root.findViewById(R.id.carlings_drunk_text)
+        drunk_text.setText(sharedPref?.getInt("carlings_drunk", 0).toString())
+
+        val carlingPlusButton: Button = root.findViewById(R.id.plus_carling)
+
+        carlingPlusButton.setOnClickListener()
+        {
+            if (drunk != null)
+            {
+                drunk = drunk!! + 1
+                sharedPref?.edit()?.putInt("carlings_drunk", drunk!!)?.apply()
+                drunk_text.setText(sharedPref?.getInt("carlings_drunk", 0).toString())
+            }
+        }
 
         return root
     }
